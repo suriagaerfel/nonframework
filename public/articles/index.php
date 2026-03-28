9bf8150e04a50de496c1f245af72b8c087aed116
suriagaerfel <suriagaerfel@gmail.com>
2026-03-28 18:48:10 +0800

    Added pages

--- public/articles/index.php
+++ public/articles/index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Non Framework - Home</title>
+    <title>Non Framework - Articles</title>
   </head>
   <body>
     <h1>Hello World! <?php echo 'Form PHP';?></h1>
