f1ba197611b3ec6c9414f576823f60ea0f9e5293
suriagaerfel <suriagaerfel@gmail.com>
2026-03-28 18:39:42 +0800

    Added public folder

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Non Framework</title>
-  </head>
-  <body>
-    <h1>Hello World! <?php echo 'Form PHP';?></h1>
-  </body>
-</html>
+
+<?php require __DIR__ . '/../public/index.php'; ?>
+
---
+++ public/index.php
@@ -0,0 +1,12 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Non Framework</title>
+  </head>
+  <body>
+    <h1>Hello World! <?php echo 'Form PHP';?></h1>
+  </body>
+</html>
